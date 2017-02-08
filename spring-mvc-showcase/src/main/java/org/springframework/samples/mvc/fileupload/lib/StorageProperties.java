2582618ba288a3579ad4475e016f2e406631e2dd
user <user@user-PC>
2017-02-08 22:09:08 +0900

    COMMIT

---
+++ spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/fileupload/lib/StorageProperties.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.mvc.fileupload.lib;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("storage")
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "c:\\upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}

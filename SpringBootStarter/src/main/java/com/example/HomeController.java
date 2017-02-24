2ef12936e5e03e8c3728c806612febc777dcb4e6
user <user@user-PC>
2017-02-24 17:33:26 +0900

    커밋

--- SpringBootStarter/src/main/java/com/example/HomeController.java
+++ SpringBootStarter/src/main/java/com/example/HomeController.java
@@ -1,15 +1,24 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bean.Home;
 
 @RestController	
 public class HomeController {
 
 	// http://cusonar.tistory.com/2
 	
-	@RequestMapping("/") 
-	public String home() {
-		return "Hello World";
+	@RequestMapping("/{name}/{message}") 
+	public Home home(
+			@PathVariable String name,
+			@PathVariable String message
+			) {
+		Home home = new Home();
+		home.setName(name);
+		home.setMessage(message);
+		return home;
 	}
 }
---
+++ SpringBootStarter/src/main/java/com/example/bean/Home.java
@@ -0,0 +1,21 @@
+package com.example.bean;
+
+public class Home {
+	
+	private String name;
+	private String message;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
